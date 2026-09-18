772f1ff2f80ed1ac52152ddf4739c7068684dd2f
jasminetagwercher-alt <jasmine.tagwercher@gmail.com>
2026-09-18 21:51:52 +0200

    Build from app template for branch Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Lernplattform_Physik_3/",
+  build: {
+    rollupOptions: {
+      input: "app.html",
+    },
+  },
 });
